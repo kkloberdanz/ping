dcee7f29747daa3d085797b71da2f1346f6c6ede
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-07-05 20:48:46 -0500

    it's finally playable!

--- main.c
+++ main.c
@@ -6,10 +6,16 @@
 #define MAX(A, B) (((A) > (B)) ? (A) : (B))
 
 enum {
-    MAX_VELOCITY = 40,
-    MIN_VELOCITY = 10,
+    MAX_VELOCITY = 20,
+    MIN_VELOCITY = 5,
     SCREEN_WIDTH = 800,
     SCREEN_HEIGHT = 600
+};
+
+enum GameState {
+    RUNNING,
+    PLAYER_SCORED,
+    ENEMY_SCORED
 };
 
 struct Entity {
@@ -57,86 +63,160 @@ bool game_running() {
     return true;
 }
 
-void set_ball_velocity(struct Entity *entity) {
-    if (entity->pos.x > SCREEN_WIDTH) {
-        entity->x_velocity = rand_ball_velocity() * (-1);
+bool obj_touching(SDL_Rect *rect1, SDL_Rect *rect2) {
+    if (rect1->x >= rect2->x + rect2->w) {
+        return false;
+    } else if (rect1->y >= rect2->y + rect2->h) {
+        return false;
+    } else if (rect2->x >= rect1->x + rect1->w) {
+        return false;
+    } else if (rect2->y >= rect1->y + rect1->h) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+enum GameState do_game_logic(
+    struct Entity *ball,
+    struct Entity *player_paddle,
+    struct Entity *enemy_paddle) {
+
+    if (obj_touching(&ball->pos, &player_paddle->pos)) {
+        ball->x_velocity = rand_ball_velocity();
+    } else if (obj_touching(&ball->pos, &enemy_paddle->pos)) {
+        ball->x_velocity = rand_ball_velocity() * (-1);
+    } else if (ball->pos.x > SCREEN_WIDTH) {
+        return PLAYER_SCORED;
+    } else if (ball->pos.x <= 0) {
+        return ENEMY_SCORED;
     }
 
-    if (entity->pos.y > SCREEN_HEIGHT) {
-        entity->y_velocity = rand_ball_velocity() * (-1);
+    if (ball->pos.y > SCREEN_HEIGHT) {
+        ball->y_velocity = rand_ball_velocity() * (-1);
+    } else if (ball->pos.y <= 0) {
+        ball->y_velocity = rand_ball_velocity();
     }
 
-    if (entity->pos.x <= 0) {
-        entity->x_velocity = rand_ball_velocity();
-    }
-
-    if (entity->pos.y <= 0) {
-        entity->y_velocity = rand_ball_velocity();
-    }
-
-    entity->pos.x += entity->x_velocity;
-    entity->pos.y += entity->y_velocity;
+    ball->pos.x += ball->x_velocity;
+    ball->pos.y += ball->y_velocity;
+    return RUNNING;
 }
 
 int game_loop() {
     SDL_Surface *backbuffer = SDL_SetVideoMode(
         SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_SWSURFACE);
 
-    struct Entity ball = {
-        .pos = {
-            .x = SCREEN_WIDTH / 2,
-            .y = SCREEN_HEIGHT / 2,
-            .h = 10,
-            .w = 10
-        },
-        .image = load_image("assets/ball.bmp"),
-        .x_velocity = 10,
-        .y_velocity = 10
-    };
+    int player_score = 0;
+    int enemy_score = 0;
 
-    struct Entity player_paddle = {
-        .pos = {
-            .x = 20,
-            .y = SCREEN_HEIGHT / 2,
-            .h = 100,
-            .w = 20
-        },
-        .image = load_image("assets/player.bmp"),
-        .x_velocity = 10,
-        .y_velocity = 20
-    };
+/* ensure tail recursion */
+restart_game: {
+        printf("player: %d, enemy: %d\n", player_score, enemy_score);
+        struct Entity ball = {
+            .pos = {
+                .x = SCREEN_WIDTH / 2,
+                .y = SCREEN_HEIGHT / 2,
+                .h = 10,
+                .w = 10
+            },
+            .image = load_image("assets/ball.bmp"),
+            .x_velocity = rand_ball_velocity(),
+            .y_velocity = rand_ball_velocity()
+        };
 
-    SDL_Surface *background_image = load_image("assets/background.bmp");
+        struct Entity player_paddle = {
+            .pos = {
+                .x = 20,
+                .y = SCREEN_HEIGHT / 2,
+                .h = 100,
+                .w = 20
+            },
+            .image = load_image("assets/player.bmp"),
+            .x_velocity = 0,
+            .y_velocity = 30
+        };
 
-    while (game_running()) {
-        SDL_BlitSurface(background_image, NULL, backbuffer, NULL);
-        SDL_BlitSurface(ball.image, NULL, backbuffer, &ball.pos);
-        SDL_BlitSurface(
-            player_paddle.image,
-            NULL,
-            backbuffer,
-            &player_paddle.pos);
+        struct Entity enemy_paddle = {
+            .pos = {
+                .x = SCREEN_WIDTH - 20,
+                .y = SCREEN_HEIGHT / 2,
+                .h = 100,
+                .w = 20
+            },
+            .image = load_image("assets/enemy.bmp"),
+            .x_velocity = 0,
+            .y_velocity = 15
+        };
 
-        unsigned char *keys = SDL_GetKeyState(NULL);
-        if (keys[SDLK_DOWN]) {
-            player_paddle.pos.y += player_paddle.y_velocity;
-            if (player_paddle.pos.y > SCREEN_HEIGHT - player_paddle.pos.h) {
-                player_paddle.pos.y = SCREEN_HEIGHT - player_paddle.pos.h;
+        SDL_Surface *background_image = load_image("assets/background.bmp");
+
+        while (game_running()) {
+            int enemy_direction = 0;
+            SDL_BlitSurface(background_image, NULL, backbuffer, NULL);
+            SDL_BlitSurface(ball.image, NULL, backbuffer, &ball.pos);
+
+            SDL_BlitSurface(
+                player_paddle.image,
+                NULL,
+                backbuffer,
+                &player_paddle.pos);
+
+            SDL_BlitSurface(
+                enemy_paddle.image,
+                NULL,
+                backbuffer,
+                &enemy_paddle.pos);
+
+
+            unsigned char *keys = SDL_GetKeyState(NULL);
+            if (keys[SDLK_DOWN]) {
+                player_paddle.pos.y += player_paddle.y_velocity;
+                if (player_paddle.pos.y > SCREEN_HEIGHT - player_paddle.pos.h) {
+                    player_paddle.pos.y = SCREEN_HEIGHT - player_paddle.pos.h;
+                }
+            } else if (keys[SDLK_UP]) {
+                player_paddle.pos.y -= player_paddle.y_velocity;
+                if (player_paddle.pos.y < 0) {
+                    player_paddle.pos.y = 0;
+                }
+            } else if (keys[SDLK_ESCAPE]) {
+                return 0;
             }
-        } else if (keys[SDLK_UP]) {
-            player_paddle.pos.y -= player_paddle.y_velocity;
-            if (player_paddle.pos.y < 0) {
-                player_paddle.pos.y = 0;
+
+            enum GameState game_state = do_game_logic(
+                &ball,
+                &player_paddle,
+                &enemy_paddle);
+
+            switch (game_state) {
+                case ENEMY_SCORED:
+                    enemy_score++;
+                    goto restart_game;
+
+                case PLAYER_SCORED:
+                    player_score++;
+                    goto restart_game;
+
+                default:
+                    break;
             }
-        } else if (keys[SDLK_ESCAPE]) {
-            return 0;
+
+            if (enemy_paddle.pos.y == ball.pos.y + 10) {
+                enemy_direction = 0;
+            } else if (enemy_paddle.pos.y < ball.pos.y + 10) {
+                enemy_direction = 1;
+            } else {
+                enemy_direction = -1;
+            }
+
+            enemy_paddle.pos.y += enemy_direction * enemy_paddle.y_velocity;
+
+            SDL_Flip(backbuffer);
+            SDL_Delay(20);
         }
-        set_ball_velocity(&ball);
-
-        SDL_Flip(backbuffer);
-        SDL_Delay(20);
+        return 0;
     }
-    return 0;
 }
 
 int main(void) {
